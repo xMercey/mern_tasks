ed329aef262b31889b205467204d6ceb83e30c8c
lara helmli <lahe8230@bht-berlin.de>
2025-08-12 13:57:04 +0200

    Model fertig

---
+++ backend/model/TaskModel.ts
@@ -0,0 +1,17 @@
+import { Schema, model} from "mongoose"
+
+export interface ITask {
+    titel: string
+    status: string
+    prioritaet: number
+}
+
+const taskModelSchema = new Schema<ITask> (
+    {
+        titel: { type: String, required: true },
+        status: { type: String, enum:["offen",  "in Bearbeitung", "erledigt"], default: "offen"},
+        prioritaet: { type: Number, min: 1, max:5, default: 3}
+    }
+)
+
+export const Task = model<ITask>("Task", taskModelSchema);
